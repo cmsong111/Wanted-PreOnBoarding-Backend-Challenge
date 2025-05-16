7fb0fca3299b80f5e3316310b249af3425a99c0c
Namju Kim <cmsong111@naver.com>
2025-05-16 16:52:50 +0900

    Fix: Cors 허용

--- src/main/kotlin/org/project/portfolio/config/WebConfig.kt
+++ src/main/kotlin/org/project/portfolio/config/WebConfig.kt
@@ -3,11 +3,19 @@ package org.project.portfolio.config
 import org.project.portfolio.auth.AuthenticationPrincipalArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(AuthenticationPrincipalArgumentResolver())
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
     }
 }
